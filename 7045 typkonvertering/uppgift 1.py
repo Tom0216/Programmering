c9e8c60abefecf8d1afa9f18f6d3f8ed42d37a22
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-09-15 15:39:36 +0200

    klar

---
+++ 7045 typkonvertering/uppgift 1.py
@@ -0,0 +1,21 @@
+"""
+text =input ("ange ditt favorittal")
+Ange ditt favorittal>? 4
+>>>text
+"4"
+>>>type(text)
+<class "str">
+
+bredd=float(input("Ange rektangelns bredd:"))
+höjd=float(input("Ange rektangelns höjd:"))
+area=bredd*höjd
+omkrets=2*(bredd + höjd)
+print("area:", area)
+print("omkrets:", omkrets)
+
+
+bas=float(input("Ange triangelns bas:"))
+höjd=float(input("Ange triangelns höjd:"))
+area=float(input("Ange triangelns area:"))
+area=2*(bas * höjd/2)
+"""
