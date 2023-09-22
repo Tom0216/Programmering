cfe0efbeb444ddd6f78d1e3b94f4605b5f46ec79
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-09-22 15:28:55 +0200

    Klar

--- 7060 Slingor while/uppgift 3.py
+++ 7060 Slingor while/uppgift 3.py
@@ -1,5 +1,5 @@
-
-
+"""
+A
 n1=1
 n2 = 1
 i = 0
@@ -11,3 +11,40 @@ while i< 30:
     i = i + 1
 print("klar")
 
+B
+n1=1
+n2 = 1
+i = 0
+tal =int(input("Hur många fibonacci tal ska skrivas ut?"))
+while i < tal:
+    print(n1)
+    next = n1 + n2
+    n1 = n2
+    n2 = next
+    i = i + 1
+
+C)
+n1=1
+n2 = 1
+i = 0
+tal =int(input("Ange övre gräns för fibonacci tal?"))
+while n1 < tal:
+    print(n1)
+    next = n1 + n2
+    n1 = n2
+    n2 = next
+    i = i + 1
+
+D)
+n1=1
+n2 = 1
+i = 0
+tal =int(input("Ange övre gräns för fibonacci tal?"))
+while n1 < tal:
+    print(n1)
+    next = n1 + n2
+    n1 = n2
+    n2 = next
+    i = i + 1
+    print (n2/n1)
+    """
