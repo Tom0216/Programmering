0faf771665e56e2c47b1f0f6ac95d93b16b7744d
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-09-25 10:47:26 +0200

    Klar

---
+++ 7061 slingor - mer while/uppgift 2.py
@@ -0,0 +1,14 @@
+print("Du har 5 gissningar")
+svar =int(input("Gissa på rätt tal"))
+antal_gissningar = 1
+while svar !=42 and antal_gissningar < 5:
+    if svar > 42:
+        svar = int(input("för mycket"))
+    elif svar <42:
+        svar = int(input("för litet"))
+    antal_gissningar = antal_gissningar +1
+if svar == 42:
+    print("rätt")
+
+print(antal_gissningar)
+print("klar")
