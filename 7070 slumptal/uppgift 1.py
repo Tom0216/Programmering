382ba08c227ef161743f5c52eba4ce6f6f5337d5
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-10-02 10:25:28 +0200

    Klar

---
+++ 7070 slumptal/uppgift 1.py
@@ -0,0 +1,38 @@
+"""
+fråga =input ("Vill du spela? j/n")
+import random
+i = 0
+while i < 2:
+    print(random.randrange(2,4))
+    i = i + 1
+input ("Förlust")
+"""
+"""
+fråga =input ("Vill du spela? j/n")
+import random
+i=0
+while i< 2:
+    print(random.randrange(2,7,4))
+    i = i + 1
+input ("Förlust")
+"""
+"""
+input ("Vill du spela? j/n")
+import random
+i= 0
+while i<2:
+    print(random.randrange(6, 7))
+    i=i + 1
+input ("vinst")
+"""
+"""
+input ("Vill du spela? j/n")
+import random
+i= 0
+while i<2:
+    print(random.randrange(4, 5))
+    i=i + 1
+input ("vinst")
+"""
+input ("vill du spela en stund? j/n")
+input ("Vad roligt att du ville spela en stund!")
