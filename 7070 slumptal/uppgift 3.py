912dca0c9a597b9dda9e57fe1fd637e9aa0c1c0f
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-10-23 11:12:35 +0200

    Påbörjad

--- 7070 slumptal/uppgift 3.py
+++ 7070 slumptal/uppgift 3.py
@@ -3,7 +3,12 @@ fråga = input ("Skriv (j) för att spela och (n) för att sluta")
 sten = 1
 sax = 2
 påse = 3
+
+
 while fråga =="j":
+    print("sten, sax eller påse")
+    spelarval = input ("sten, sax or påse")
+
     t1 = random.randrange (1, 4)
     if t1==1:
         print ("sten")
@@ -11,6 +16,19 @@ while fråga =="j":
         print ("sax")
     elif t1==3:
         print("påse")
-    fråga = input ("Skriv (j) för att spela och (n) för att sluta")
 
-svar = input("sten, sax eller påse")
+    if t1 == sten and spelarval == "påse":
+        print ("spelarens alternativ: påse")
+        print ("datorns alternativ: sten")
+        print ("spelare vann")
+    elif spelarval =="sten" and t1== påse:
+        print ("Dator vann")
+    elif t1== sax and spelarval == "sten":
+        print ("spelare vann")
+    elif spelarval == "sax" and t1 == sten:
+        print ("Dator vann")
+    elif t1== påse and spelarval == "sax":
+        print ("spelare vann")
+    elif t1== sax and spelarval== "påse":
+        print ("Dator vann")
+    fråga = input ("Skriv (j) för att spela och (n) för att sluta")
