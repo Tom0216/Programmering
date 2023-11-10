6c3167f4c82ccd9c14aa09aac44cac54cc11846d
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-11-10 15:34:15 +0100

    Klar

--- 7073 Flödesdiagram/uppgift 2.py
+++ 7073 Flödesdiagram/uppgift 2.py
@@ -1,6 +1,13 @@
-i= 0
-while i<5:
-    print (i)
-    i=i+1
-print("klar")
 
+text = input("Ange två heltal: ")
+tal_1 = int(text)
+text = input("Ange två heltal: ")
+tal_2 = int(text)
+
+print(tal_1, tal_2)
+
+if tal_1>tal_2:
+    print (tal_1)
+elif tal_1<tal_2:
+    print (tal_2)
+print ("klart")
