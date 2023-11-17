9f5e45dc3fd35639a149bab3ff9c12f745edbd1c
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-11-17 15:03:19 +0100

    Klar

---
+++ 7095 for slingor med range/uppgift 1.py
@@ -0,0 +1,18 @@
+"""
+for i in range(5):
+    print (i)
+"""
+"""
+for i in range (3, 8, 1):
+    print (i)
+"""
+"""
+for i in range (4, 20, 3):
+    print (i)
+"""
+"""
+for i in range (20, 41, 5):
+    print (i)
+"""
+for i in range (10, -1, -1):
+    print (i)
