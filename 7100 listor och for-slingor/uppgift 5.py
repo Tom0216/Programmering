52bd570101d724eebdb43431fc3c1cf472912ed7
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-12-01 15:51:14 +0100

    Klar

---
+++ 7100 listor och for-slingor/uppgift 5.py
@@ -0,0 +1,13 @@
+
+
+import random
+tärningar = [5]
+
+if 
+
+for i in range (4):
+    tärningar.append(random.randint(1,6))
+print ("",tärningar ,"")
+tärningar.sort()
+print("antal ettor", tärningar.count(1))
+
