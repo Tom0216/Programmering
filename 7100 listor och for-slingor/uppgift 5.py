96c5733bcfa5ca022e14c186687feced0e082748
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-12-04 10:53:16 +0100

    Klar

--- 7100 listor och for-slingor/uppgift 5.py
+++ 7100 listor och for-slingor/uppgift 5.py
@@ -1,13 +1,16 @@
 
 
 import random
-tärningar = [5]
+t1 = [(random.randint(1,6))]
 
-if 
+for i in range(4):
+    t1.append(random.randint(1,6))
+print ("",t1 ,"")
+t1.sort()
+print("antal ettor", t1.count(1))
 
-for i in range (4):
-    tärningar.append(random.randint(1,6))
-print ("",tärningar ,"")
-tärningar.sort()
-print("antal ettor", tärningar.count(1))
 
+if t1[0] == t1[1] and t1[0] == t1[2] and t1[0]==[3] and t1[0] ==t1[4]:
+    print ("yatsy 50")
+else:
+    print ("inte yatsy")
