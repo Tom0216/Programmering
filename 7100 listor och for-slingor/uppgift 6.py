9afc71078ce546db7fcdad4e160b0749a22678fb
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2023-12-08 15:42:52 +0100

    Klar

--- 7100 listor och for-slingor/uppgift 6.py
+++ 7100 listor och for-slingor/uppgift 6.py
@@ -3,14 +3,18 @@ kod= "lwthni_lnqqfw_qtb_bjnlmy_utqdstrnfq_rzqynuqjx"
 svar = ""
 Bokstäver = "abcdefghijklmnopqrstuvwxyz"
 chiffer= "zyxwvutsrqonmlkjihgfedcba"
-svar = ""
 
-for bokstav in kod:
-    tal = ord(bokstav)    
-    print(tal, bokstav)
-    tal = tal + 10
-    if tal > ord("z"):
-        tal = tal - 26
-    ny_bokstav = chr(tal)
-    svar = svar + ny_bokstav
-print (svar)
+for index in range(1,27):
+    svar = ""
+    for bokstav in kod:
+        if bokstav == "_":
+            svar = svar + bokstav
+        else:
+            tal = ord(bokstav)    
+            #print(tal, bokstav)
+            tal = tal + index
+            if tal > ord("z"):
+                tal = tal - 26
+            ny_bokstav = chr(tal)
+            svar = svar + ny_bokstav
+    print (svar)
