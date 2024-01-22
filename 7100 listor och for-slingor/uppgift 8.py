67f1580e2918728e7730070611572b21fd13ef02
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2024-01-22 11:04:32 +0100

    Påbörjad

--- 7100 listor och for-slingor/uppgift 8.py
+++ 7100 listor och for-slingor/uppgift 8.py
@@ -17,14 +17,30 @@ Kåk= ("Kåk")
 Chans= ("Chans")
 Yatsy= ("Yatsy")
 
+tärningar = []
+for i in range(1, 7):
+    tärningar.append(i)
+random.shuffle(tärningar)
+print (tärningar)
 
+print("1. ettor")
+print("2. tvåor")
+print("3. treor")
+print("4.fyror")
+print ("5. femmor")
+print ("6. sexor")
 
-t1 = random.randrange(1,6)
-t2 = random.randrange(1,6)
-t3 = random.randrange(1,6)
-t4 = random.randrange (1,6)
-t5 = random.randrage (1,6)
-
-
-
-
+svar = input("välj: ")
+if svar == "1":
+    ("antal ettor", tärningar.count(1))
+elif svar == "2":
+    ("antal tvår", tärningar.count(2))
+elif svar == 3:
+    ("antal treor", tärningar.count (3))
+elif svar ==4:
+    ("antalet fyror", tärningar.count (4))
+elif svar == 5:
+    ("antalet femmor", tärningar.count (5))
+elif svar == 6:
+    ("antalet sexor", tärningar.count (6))
+hand = []
