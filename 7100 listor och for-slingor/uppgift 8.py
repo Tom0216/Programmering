a0bd59c8d87d242d372e842eb4663c932fa7d0c7
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2024-02-02 16:07:37 +0100

    Påbörjad

--- 7100 listor och for-slingor/uppgift 8.py
+++ 7100 listor och for-slingor/uppgift 8.py
@@ -17,44 +17,171 @@ Kåk= ("Kåk")
 Chans= ("Chans")
 Yatsy= ("Yatsy")
 
-tärningar = []
-# upprepa 5 gånger
-# kasta tärning (slumpa) lägg till i tärningar
+sparade_alternativ = {'ettor' : -1, 'tvåor' : -1, 'treor' : -1, 'fyror' : -1, 'femmor': -1,'sexor':-1 }
+print(sparade_alternativ)
 
-slumptal = random.randrange(1, 7)
-tärningar.append(slumptal)
-slumptal = random.randrange(1, 7)
-tärningar.append(slumptal)
-slumptal = random.randrange(1, 7)
-tärningar.append(slumptal)
-slumptal = random.randrange(1, 7)
-tärningar.append(slumptal)
-slumptal = random.randrange(1, 7)
-tärningar.append(slumptal)
+for j in range(6):
+    tärningar = []
+    # upprepa 5 gånger
+    # kasta tärning (slumpa) lägg till i tärningar
 
-print (tärningar)
+    slumptal = random.randrange(1, 7)
+    tärningar.append(slumptal)
+    slumptal = random.randrange(1, 7)
+    tärningar.append(slumptal)
+    slumptal = random.randrange(1, 7)
+    tärningar.append(slumptal)
+    slumptal = random.randrange(1, 7)
+    tärningar.append(slumptal)
+    slumptal = random.randrange(1, 7)
+    tärningar.append(slumptal)
 
-# upprepa 5 gånger
-# kasta tärning (slumpa) lägg till i tärningar
+    print (tärningar)
+    for i in range(2):
+        # välj vilka tärningar som ska sparas
+        print("1. ettor")
+        print("2. tvåor")
+        print("3. treor")
+        print("4.fyror")
+        print ("5. femmor")
+        print ("6. sexor")
+
+        svar = input("välj: ")
+        if svar == "1":
+            # [2, 1, 4, 1, 2]
+            antal_ettor = tärningar.count(1)
+            tärningar = []
+            for i in range(antal_ettor):
+                tärningar.append (1)
+            # [1, 1]
+            # kasta övriga
+            for i in range (5-antal_ettor):
+                slumptal = random.randrange(1, 7)
+                tärningar.append(slumptal)
+            print("debug", tärningar)
+            print("antal ettor:", tärningar.count(1))
+            print("1",tärningar.count(1) )
+        elif svar == "2":
+            # [2, 1, 4, 1, 2]
+            antal_ettor = tärningar.count(1)
+            tärningar = []
+            for i in range(antal_ettor):
+                tärningar.append (1)
+            # [1, 1]
+            # kasta övriga
+            for i in range (5-antal_ettor):
+                slumptal = random.randrange(1, 7)
+                tärningar.append(slumptal)
+            print("debug", tärningar)
+            print("antal tvåor:", tärningar.count(2)) 
+            print("2",  tärningar.count(2) * 2)
+        elif svar == "3":
+            # [2, 1, 4, 1, 2]
+            antal_ettor = tärningar.count(1)
+            tärningar = []
+            for i in range(antal_ettor):
+                tärningar.append (1)
+            # [1, 1]
+            # kasta övriga
+            for i in range (5-antal_ettor):
+                slumptal = random.randrange(1, 7)
+                tärningar.append(slumptal)
+            print("debug", tärningar)
+            print("antal treor:", tärningar.count (3)) 
+            print("3", tärningar.count (3) *3 )
+        elif svar == "4":
+            # [2, 1, 4, 1, 2]
+            antal_fyror = tärningar.count(4)
+            tärningar = []
+            for i in range(antal_fyror):
+                tärningar.append (4)
+            # [1, 1]
+            # kasta övriga
+            for i in range (5-antal_fyror):
+                slumptal = random.randrange(1, 7)
+                tärningar.append(slumptal)
+            print("debug", tärningar)
+            print("antal fyror:", tärningar.count (4)) 
+            print("4",tärningar.count (4)*4 )
+        elif svar == "5":
+            # [2, 1, 4, 1, 2]
+            antal_femmor = tärningar.count(5)
+            tärningar = []
+            for i in range(antal_femmor):
+                tärningar.append (5)
+            # [1, 1]
+            # kasta övriga
+            for i in range (5-antal_femmor):
+                slumptal = random.randrange(1, 7)
+                tärningar.append(slumptal)
+            print("debug", tärningar)
+            print("antal femmor:", tärningar.count (5)) 
+            print("5",tärningar.count (5)*5 )
+        elif svar == "6":
+            # [2, 1, 4, 1, 2]
+            antal_sexor = tärningar.count(6)
+            tärningar = []
+            for i in range(antal_sexor):
+                tärningar.append (6)
+            # [1, 1]
+            # kasta övriga
+            for i in range (5-antal_sexor):
+                slumptal = random.randrange(1, 7)
+                tärningar.append(slumptal)
+            print("debug", tärningar)
+            print("antal sexor:", tärningar.count (6)) 
+            print("6", tärningar.count (6) * 6 )
 
 
-print("1. ettor")
-print("2. tvåor")
-print("3. treor")
-print("4.fyror")
-print ("5. femmor")
-print ("6. sexor")
+    # omgång 1 klar
+    # välj vad det ska sparas som
+    # meny
+    # spara i variabel
+    print("Vad vill du spara det som?")
+    print("1. ettor")
+    print("2. tvåor")
+    print("3. treor")
+    print("4.fyror")
+    print ("5. femmor")
+    print ("6. sexor")
 
-svar = input("välj: ")
-if svar == "1":
-    print("antal ettor:", tärningar.count(1))
-elif svar == "2":
-    print("antal tvåor:", tärningar.count(2))
-elif svar == "3":
-    print("antal treor:", tärningar.count (3))
-elif svar == "4":
-    print("antal fyror:", tärningar.count (4))
-elif svar == "5":
-    print("antal femmor:", tärningar.count (5))
-elif svar == "6":
-    print("antal sexor:", tärningar.count (6))
+    svar = input("välj: ")
+
+    if svar == "1":
+        sparade_alternativ ["ettor"] = tärningar.count (1) * 1
+    if svar == "2":
+        sparade_alternativ ["tvåor"] = tärningar.count (2) * 2
+    if svar == "3":
+        sparade_alternativ ["treor"] = tärningar.count (3) * 3
+    if svar == "4":
+        sparade_alternativ ["fyror"] = tärningar.count (4) * 4
+    if svar == "5":
+        sparade_alternativ ["femmor"] = tärningar.count (5) * 5
+    if svar == "6":
+        sparade_alternativ ["sexor"] = tärningar.count (6) * 6
+
+    print(sparade_alternativ)
+
+
+
+"""
+    svar = input("välj: ")
+    if svar == "1":
+        print("antal ettor:", tärningar.count(1))
+        print("1",tärningar.count(1) )
+    elif svar == "2":
+        print("antal tvåor:", tärningar.count(2)) 
+        print("2",  tärningar.count(2) * 2)
+    elif svar == "3":
+        print("antal treor:", tärningar.count (3)) 
+        print("3", tärningar.count (3) *3 )
+    elif svar == "4":
+        print("antal fyror:", tärningar.count (4)) 
+        print("4",tärningar.count (4)*4 )
+    elif svar == "5":
+        print("antal femmor:", tärningar.count (5)) 
+        print("5",tärningar.count (5)*5 )
+    elif svar == "6":
+        print("antal sexor:", tärningar.count (6)) 
+        print("6", tärningar.count (6) * 6 )
+"""
