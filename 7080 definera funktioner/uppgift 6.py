42fe46eafc74a62a8246f625adf9f551d30ffb95
Tom Hallström <tom.hallstrom06@skola.stenungsund.se>
2024-04-29 11:08:55 +0200

    Klar

--- 7080 definera funktioner/uppgift 6.py
+++ 7080 definera funktioner/uppgift 6.py
@@ -1,2 +1,13 @@
-def summa (jämnt_or_udda):
+def är_jämnt (tal):
+    if tal % 2 == 0:
+        return True
+    else:
+        return False
     
+mitt_tal = int(input("ange ett heltal: "))
+
+if är_jämnt(mitt_tal):
+    print("jämnt")
+else:
+    print("udda")
+
